ea557526bb7f967b2d8a28ac239019728ff30505
hwright-ucsb <wright@umail.ucsb.edu>
2018-02-14 10:18:09 -0500

    add similar config file from DT method paper

---
+++ data/config_sq_1_1.py
@@ -0,0 +1,22 @@
+# Configuration file from 
+#	"A Delaunay Triangulation based method for 
+# 	 wireless sensor network deployment" 2007
+#		C-H Wu, K-C Lee, Y-C Chung
+
+
+#######
+# For initial experiments, use their rectangular region
+
+# Shapes: one per line, first # = sides
+#		  following #s coords  x y x y ...
+
+
+# size of sensing area
+4 0 0 0 400 400 400 400 0
+# number of obstacles/holes
+1
+# position of obstacles
+4 101 74 87 93 99 119 119 95
+# number of pre-deployed sensors
+0
+# position of pre-deployed sensors
